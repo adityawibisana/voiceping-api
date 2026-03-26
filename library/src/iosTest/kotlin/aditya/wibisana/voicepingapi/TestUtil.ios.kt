1cd4a9319a2e78e902c908091e44846da0704000
Aditya Wibisana <aditya.wibisana@gmail.com>
2026-03-26 16:59:18 +0700

    Remove ios specific

--- library/src/iosTest/kotlin/aditya/wibisana/voicepingapi/TestUtil.ios.kt
+++ library/src/iosTest/kotlin/aditya/wibisana/voicepingapi/TestUtil.ios.kt
@@ -3,9 +3,7 @@ package aditya.wibisana.voicepingapi
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.interpretCPointer
-import platform.Foundation.*
-import platform.Security.SecTrustRef
+import platform.Foundation.setHTTPShouldUsePipelining
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun getTestEngine(): HttpClientEngine? {
@@ -14,18 +12,6 @@ actual fun getTestEngine(): HttpClientEngine? {
             setAllowsCellularAccess(true)
             setTimeoutInterval(60.0)
             setHTTPShouldUsePipelining(false)
-        }
-        handleChallenge { _, _, challenge, completionHandler ->
-            val serverTrustObj = challenge.protectionSpace.valueForKey("serverTrust")
-            val serverTrust: SecTrustRef? = serverTrustObj?.let { interpretCPointer(it.objcPtr()) }
-            if (serverTrust != null) {
-                completionHandler(
-                    NSURLSessionAuthChallengeUseCredential,
-                    NSURLCredential.credentialForTrust(serverTrust)
-                )
-            } else {
-                completionHandler(NSURLSessionAuthChallengeCancelAuthenticationChallenge, null)
-            }
         }
     }
 }
